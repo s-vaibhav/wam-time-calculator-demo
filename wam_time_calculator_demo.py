21e4eb0a80da725780e4f69452b1173e9a8b108c
s-vaibhav <134908729+s-vaibhav@users.noreply.github.com>
2023-08-27 21:59:38 +1000

    Update wam_time_calculator_demo.py

--- wam_time_calculator_demo.py
+++ wam_time_calculator_demo.py
@@ -1,5 +1,3 @@
-# 23 Feb 2023
-
 import streamlit as st
 
 st.title("WAM Time Calculator")
@@ -10,8 +8,6 @@ and preparing for quizes and exams. This number would also help you understand
 how fast or slow you need to work.''', language=None)
 
 st.markdown('''---''')
-col1, col2 = st.columns(2)
-
 
 st.markdown("#### 1. WAM Time equation (as recommended by the university)")
 st.code("50 WAM = 10 hours per unit per week")
@@ -19,7 +15,6 @@ st.code("50 WAM = 10 hours per unit per week")
 st.markdown("#### 2. Select desired WAM for this trimester to estimate the hours required per unit per week")
 slide1 = st.slider("Desired WAM", 50, 100, step=5)
 slide2 = st.slider("Hours per unit per week required", 10, 20, value=slide1//5, disabled=True)
-
 
 st.markdown("#### 3. Select number of units taken for this trimester to add to the calculation")
 slide3 = st.slider("Number of units", 1, 6, step=1)
@@ -33,7 +28,6 @@ st.markdown("#### Leave your feedback")
 radio = st.radio("Was this app helful to you?", ["Yes :)", "No :("])
 st.text_area("Comments (optional)")
 feedbacksubmit_button = st.button("Submit", key=2)
-
 if feedbacksubmit_button:
     if radio == "Yes :)":
         st.success("This is a demo app, feedback doesn't actually get submitted.")
